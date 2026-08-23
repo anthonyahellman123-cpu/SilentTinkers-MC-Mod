53bda05821233ce4d36c5b846832243d7b2174f0
anthonyahellman123-cpu <anthonyahellman123@gmail.com>
2026-08-23 11:24:00 -0700

    Teach Silent Gear charger about composite tools

---
+++ src/main/java/com/anthonyahellman/silenttinkers/mixin/ChargerInputSlotMixin.java
@@ -0,0 +1,18 @@
+package com.anthonyahellman.silenttinkers.mixin;
+
+import com.anthonyahellman.silenttinkers.compat.silentgear.CompositeStarChargeService;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/** Allows validated composite parts/tools into the charger's input GUI slot. */
+@Mixin(targets = "net.silentchaos512.gear.block.charger.ChargerContainer$1", remap = false)
+public abstract class ChargerInputSlotMixin {
+    @Inject(method = "mayPlace", at = @At("HEAD"), cancellable = true)
+    private void silenttinkers$allowCompositeInput(ItemStack stack,
+                                                    CallbackInfoReturnable<Boolean> callback) {
+        if (CompositeStarChargeService.canBeginCharging(stack)) callback.setReturnValue(true);
+    }
+}
