b65863ee8a9cf02716edda6b81785eef9356d7d3
anthonyahellman123-cpu <anthonyahellman123@gmail.com>
2026-08-23 19:51:11 -0700

    Use consistent player-facing alloy composition labels

--- src/main/java/com/anthonyahellman/silenttinkers/fluid/CompositeAlloyFluidType.java
+++ src/main/java/com/anthonyahellman/silenttinkers/fluid/CompositeAlloyFluidType.java
@@ -2,15 +2,14 @@ package com.anthonyahellman.silenttinkers.fluid;
 
 import com.anthonyahellman.silenttinkers.client.AlloyVisualColorResolver;
 import com.anthonyahellman.silenttinkers.client.SourceVisualColorResolver;
+import com.anthonyahellman.silenttinkers.material.AlloyDisplayFormatter;
 import com.anthonyahellman.silenttinkers.material.AlloyPayload;
-import com.anthonyahellman.silenttinkers.material.MaterialIngredient;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidType;
 
-import java.util.Locale;
 import java.util.function.Consumer;
 
 /** Dynamic molten alloy name and client renderer backed by Tinkers' neutral molten sprites. */
@@ -26,38 +25,9 @@ public final class CompositeAlloyFluidType extends FluidType {
 
     @Override
     public Component getDescription(FluidStack stack) {
-        return AlloyPayload.read(stack).<Component>map(composition -> {
-            if (composition.ingredients().size() == 1) {
-                return Component.literal("Molten " + displayName(composition.ingredients().get(0)) + " (100%)");
-            }
-            if (composition.ingredients().size() <= 3) {
-                String shares = composition.ingredients().stream()
-                        .map(ingredient -> displayName(ingredient) + " " + String.format(Locale.ROOT, "%.1f%%",
-                                100.0 * ingredient.units() / composition.totalUnits()))
-                        .reduce((left, right) -> left + " / " + right)
-                        .orElse("Composite");
-                return Component.literal("Molten Alloy (" + shares + ")");
-            }
-            return Component.literal("Molten Composite Alloy (" + composition.ingredients().size() + " Materials)");
-        }).orElseGet(() -> super.getDescription(stack));
-    }
-
-    private static String displayName(MaterialIngredient ingredient) {
-        String path = ingredient.materialId().getPath();
-        int slash = path.lastIndexOf('/');
-        if (slash >= 0) path = path.substring(slash + 1);
-        StringBuilder result = new StringBuilder(path.length());
-        boolean capitalize = true;
-        for (char character : path.toCharArray()) {
-            if (character == '_') {
-                result.append(' ');
-                capitalize = true;
-            } else {
-                result.append(capitalize ? Character.toUpperCase(character) : character);
-                capitalize = false;
-            }
-        }
-        return result.toString();
+        return AlloyPayload.read(stack)
+                .<Component>map(composition -> Component.literal(AlloyDisplayFormatter.moltenLabel(composition)))
+                .orElseGet(() -> super.getDescription(stack));
     }
 
     @Override
